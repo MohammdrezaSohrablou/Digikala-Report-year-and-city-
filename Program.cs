678c5f5d8d6f33c94054fbe1f9c03a4a68c5364b
MohammdrezaSohrablou <127177875+MohammdrezaSohrablou@users.noreply.github.com>
2023-03-15 13:58:43 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
-﻿using System.IO;
+using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string address = @"D:\digikala report\orders.csv";
+        string address = @"D:\digikalaprogram\orders.csv";
+       
         DigiKalaReport x = new DigiKalaReport();
 
         Console.WriteLine("processing...");
         Console.WriteLine("please wait, don't close -cmd-'");
-        x.CityReport(address);
+        //x.CityReport(address);
         x.YearsReport(address);
         Console.WriteLine("done !");
     }
@@ -21,129 +23,84 @@ class DigiKalaReport
 {
     public void CityReport(string address)
     {
-        List<string> citys = new List<string>();
-        
-        using (StreamReader srreader = new StreamReader(address))
+        var citySet = new HashSet<string>();
+        var lines = File.ReadAllLines(address);
+
+        foreach (var line in lines)
         {
-            while (!srreader.EndOfStream)
-            {
-                string line = srreader.ReadLine();
-                
-                string[] fields = line.Split(',');
-                
-                string cityName = Regex.Replace(fields[5], @"(\d|\.|,|:|-| )+", "");
-                
-                citys.Add(cityName);
-            }
+            string[] fields = line.Split(',');
+
+            string cityName = Regex.Replace(fields[5], @"(\d|\.|,|:|-| )+", "");
+            citySet.Add(cityName);
         }
 
-        var nonduplicatecitys = citys.Distinct();
-
-        
-        foreach (string city in nonduplicatecitys)
+        foreach (string city in citySet)
         {
-            
-            string addressOutput = @"D:\digikala report\City\orders.csv";
-            using (StreamWriter strwriter = new StreamWriter(addressOutput + city + ".csv"))
+            string outputDirectory = Path.Combine(@"D:\digikalaprogram\city\", city);
+            if (Directory.Exists(outputDirectory))
             {
-                
-                strwriter.WriteLine("ID_Order,ID_Customer,ID_Item,DateTime_CartFinalize,Amount_Gross_Order,city_name_fa,Quantity_item");
+                Directory.Delete(outputDirectory, true);
+            }
+            Directory.CreateDirectory(outputDirectory);
 
-                
-                using (StreamReader srreader = new StreamReader(address))
+            string outputFile = Path.Combine(outputDirectory, "orders.csv");
+
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                writer.WriteLine("ID_Order,ID_Customer,ID_Item,DateTime_CartFinalize,Amount_Gross_Order,city_name_fa,Quantity_item");
+
+                foreach (var line in lines)
                 {
-                    while (!srreader.EndOfStream)
+                    string[] fields = line.Split(',');
+                    string cityName = Regex.Replace(fields[5], @"(\d|\.|,|:|-| )+", "");
+
+                    if (cityName == city)
                     {
-                        string line = srreader.ReadLine();
-                        
-                        string[] fields = line.Split(',');
-                        
-                        string cityName = Regex.Replace(fields[5], @"(\d|\.|,|:|-| )+", "");
-                        
-                        if (cityName == city)
-                        {
-                            
-                            strwriter.WriteLine($"{fields[0]},{fields[1]},{fields[2]},{fields[3]},{fields[4]},{fields[5]},{fields[6]}");
-                        }
+                        writer.WriteLine($"{fields[0]},{fields[1]},{fields[2]},{fields[3]},{fields[4]},{fields[5]},{fields[6]}");
                     }
                 }
             }
         }
     }
-
-
-
-
 
     public void YearsReport(string address)
     {
-        List<string> years = new List<string>();
-        
-        using (StreamReader srreader = new StreamReader(address))
+        var yearSet = new HashSet<string>();
+        var lines = File.ReadAllLines(address);
+
+        foreach (var line in lines)
         {
-            while (!srreader.EndOfStream)
-            {
-                string line = srreader.ReadLine();
-               
-                string[] fields = line.Split(',');
-                
-                string date = Regex.Replace(fields[3], @"(\d{4}\-)+", "");
-                
-                years.Add(date);
-            }
+            string[] fields = line.Split(',');
+            string date = Regex.Replace(fields[3], @"(\d{4}\-)+", "");
+            yearSet.Add(date);
         }
 
-        var nonduplicateyears = years.Distinct();
-
-        
-        foreach (string dateyear in nonduplicateyears)
+        foreach (string year in yearSet)
         {
-            
-            string addressOutput = @"D:\digikala report\Years\orders.csv";
-            using (StreamWriter strwriter = new StreamWriter(addressOutput + dateyear + ".csv"))
+            var outputDirectory = Path.Combine(@"D:\digikalaprogram\years\", year);
+            if (Directory.Exists(outputDirectory))
             {
-                
-                strwriter.WriteLine("ID_Order,ID_Customer,ID_Item,DateTime_CartFinalize,Amount_Gross_Order,city_name_fa,Quantity_item");
+                Directory.Delete(outputDirectory, true);
+            }
+            _ = Directory.CreateDirectory(outputDirectory);
 
-                
-                using (StreamReader srreader = new StreamReader(address))
+            string outputFile = Path.Combine(outputDirectory, "orders.csv");
+
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                writer.WriteLine("ID_Order,ID_Customer,ID_Item,DateTime_CartFinalize,Amount_Gross_Order,city_name_fa,Quantity_item");
+
+                foreach (var line in lines)
                 {
-                    while (!srreader.EndOfStream)
-                    {
-                        string line = srreader.ReadLine();
+                    string[] fields = line.Split(',');
+                    string date = Regex.Replace(fields[3], @"(\d{4}\-)+", "");
 
-                        string[] fields = line.Split(',');
-                        string date = Regex.Replace(fields[3], @"(\d{4}\-)+", "");
-                        if (date == dateyear)
-                        {
-                            strwriter.WriteLine($"{fields[0]},{fields[1]},{fields[2]},{fields[3]},{fields[4]},{fields[5]},{fields[6]}");
-                        }
+                    if (date == year)
+                    {
+                        writer.WriteLine($"{fields[0]},{fields[1]},{fields[2]},{fields[3]},{fields[4]},{fields[5]},{fields[6]}");
                     }
                 }
             }
         }
-
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
